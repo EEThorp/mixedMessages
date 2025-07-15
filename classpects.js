21505cbc6c8e0504d0f7f2afce971485fda2c663
Liz Thorp <liz.thorp@hey.com>
2025-07-15 13:31:26 +0100

    Create classpects.js
    arrays of classes and aspects

---
+++ classpects.js
@@ -0,0 +1,5 @@
+// arrays for classes and aspects
+
+const classHS = ["Thief", "Rogue", "Bard", "Prince", "Heir", "Page", "Seer", "Maid", "Sylph", "Knight", "Witch", "Mage"]
+
+const aspects = ["Space", "Time", "Mind", "Heart", "Hope", "Rage", "Breath", "Blood", "Life", "Doom", "Light", "Void"];
